d56aa89f138bbbaf00652287a97b985361f5f1ad
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2024-01-17 08:08:24 +0200

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,11 @@
+# day 2 of python
+# there are different data types in python like strings, integers, floats, booleans
+# strings
+print("Hello"[-1])
+# this is called indexing and allows you to access individual characters in a string
+# even tho you can you can put a number in a string , it will still be a string data type and won't be seen as a number
+print("123" + "456")
+# to convert a string to a number use int() or float()
+# int() converts to an integer
+#integer
+print(123 +345)
